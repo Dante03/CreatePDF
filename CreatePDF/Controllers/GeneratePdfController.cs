a8bbf14332c0479615d1f906966a7a6b95ea12ba
Gerardo Reyes <gerardo.reyes@tuidentidad.com>
2023-07-07 09:34:17 -0600

    Agregar archivos de proyecto.

---
+++ CreatePDF/Controllers/GeneratePdfController.cs
@@ -0,0 +1,47 @@
+﻿using CreatePDF.Helpers;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CreatePDF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneratePdfController : ControllerBase
+    {
+        // GET: api/<CreatePdfController>
+
+        private IConverter _converter;
+        public GeneratePdfController(IConverter converter)
+        {
+            _converter = converter;
+        }
+        [HttpPost("{reportname}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<string> Post([FromRoute] string reportname)
+        {
+            string teamplateName = reportname switch
+            {
+                "ine" => "Views/inepdf.html",
+                "cfe" => "Views/cfepdf.html",
+                "rfc" => "Views/rfcpdf.html",
+                "curp" => "Views/curppdf.html",
+                "wh" => "Views/workhisotrypdf.html",
+                "legal" => "Views/legalOpinionpdf.html",
+                "bgc" => "Views/bgcpdf.html",
+                "idi" => "Views/idi.html",
+                _ => string.Empty
+            };
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            string html = GeneratePDFHelper.Template(dic , teamplateName);
+            byte[] file = _converter.Convert(GeneratePDFHelper.HtmlToPDF(html));
+            return File(file, MediaTypeNames.Application.Pdf, $"{Guid.NewGuid()}.pdf");
+        }
+    }
+}
---
+++ CreatePDF/Helpers/GeneratePDFHelper.cs
@@ -0,0 +1,63 @@
+﻿using DinkToPdf;
+
+namespace CreatePDF.Helpers
+{
+    public static class GeneratePDFHelper
+    {
+        public static HtmlToPdfDocument HtmlToPDF(string htmlContent)
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 0, Bottom = 4, Left = 0, Right = 0 },
+                DocumentTitle = "Test",
+                
+            };
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = htmlContent,
+                WebSettings = {
+                    DefaultEncoding = "utf-8"
+                },
+                FooterSettings = {
+                    FontSize = 7,
+                    Left = "Este documento es proporcionado para la verificación de la información ingresada. No es un documento oficial.",
+                    Right = "pag [page]/[toPage]",
+                    Line = false,
+                },
+            };
+
+            var htmlToPdfDocument = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings },
+            };
+            return htmlToPdfDocument;
+        }
+        public static string Template(Dictionary<string, string> dictionary, string filePath)
+        {
+            string rootpath = $"{AppDomain.CurrentDomain.BaseDirectory}";
+            var path = Path.Combine(rootpath, filePath);
+
+            //Read file content
+            string htmlContent = File.ReadAllText(path);
+
+            string klibuimage = $"{rootpath}Views/Images/logocolor.jpg";
+            Byte[] klibubytes = System.IO.File.ReadAllBytes(klibuimage);
+            dictionary.Add("[KLIBU]", $"data:image;base64,{Convert.ToBase64String(klibubytes)}");
+
+            // Set data
+            foreach (var v in dictionary)
+            {
+                htmlContent = htmlContent.Replace(
+                    v.Key,
+                    v.Value
+                );
+            }
+            return htmlContent;
+        }
+    }
+}
